3b7f50d9e84a7d9415622acf4c7595abcb944a54
ZuberDunge <iamZuberDunge@gmail.com>
2022-04-05 11:16:00 +0530

    update : removed commented code

--- src/user/login.js
+++ src/user/login.js
@@ -31,9 +31,6 @@ export default function Login() {
 
                     <form onSubmit={handleSubmit(signUpUser)}>
                         <div class="input-parent">
-                            {/* <input type="text" required onChange={(event) => {
-                        setuserName(event.target.value);
-                    }} /> */}
                             <label for="name">Enter your name </label>
                             <input
                                 {...register("userName", {
@@ -46,19 +43,12 @@ export default function Login() {
                             {errors?.userName?.type === "miLength" && (
                                 <span><i class="fas fa-exclamation-triangle"></i> name should be atleast 2 characters</span>
                             )}
-
                             {errors?.userName?.type === "pattern" && (
                                 <span> <i class="fas fa-exclamation-triangle"></i> Alphabetical characters only</span>
                             )}
-
-
                         </div>
 
-
                         <div class="input-parent">
-                            {/* <input type="text" required onChange={(event) => {
-                        setRegisterEmail(event.target.value);
-                    }} /> */}
                             <label for="email">Enter your email </label>
                             <input
                                 {...register("registerEmail", {
@@ -68,17 +58,10 @@ export default function Login() {
                             />
                             {errors?.registerEmail?.type === "pattern" && <span><i class="fas fa-exclamation-triangle"></i> Enter valid Email</span>}
                             {errors?.registerEmail?.type === "required" && <span><i class="fas fa-exclamation-triangle"></i> This field is required</span>}
-
-
-
                         </div>
 
                         <div class="input-parent">
                             <label for="Password">Enter Password </label>
-                            {/* <input type={passVisible ? "password" : "text"} required
-                        onChange={(event) => {
-                            setRegisterPassword(event.target.value);
-                        }} /> */}
                             <input type={passVisible ? "password" : "text"} {...register("registerPassword", { minLength: 6, required: true, pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/ })} />
                             <div onClick={() => setPassVisible(!passVisible)}
                                 class={`pointer hide-pass-icon text-color-grey fas ${passVisible ? "fa-eye-slash" : "fa-eye"}`}></div>
@@ -88,84 +71,58 @@ export default function Login() {
                         )}
                         {errors?.registerPassword?.type === "required" && <span></span>}
 
-
-
-
-                        {/* <p class="small-text">
-                    By continuing, your agree to BatStore's Terms of Use and Privacy
-                    Policy.
-                </p> */}
                         <button class="bat-btn plcae-order-btn bat-br-3px bat-btn-primary">
                             Sign Up
                         </button>
                     </form>
+
                     <div class="bat-fs-1r bat-txt-center margin-t-1 bat-pad-tb-1 ">
                         Already a member?
                         <span class="text-color-primary pointer bat-fw-700" onClick={() => setNewUser(false)}> Log In! </span>
                     </div>
                     <button class="bat-btn bat-br-3px bat-btn-primary-outlined bat-fs-14px" style={{ color: "white" }} onClick={loginAsGuest}> Sign in as Guest</button>
-
-
-
-
-
                 </div>
-
                 </>
                 :
+                <>
+                    <div class="bat-login-form">
+                        <h3 class="bat-m-tb-1r bat-txt-center ">Welcome Back 👋🏻</h3>
+                        <form onSubmit={handleSubmit(signInUser)}>
+                            <div class="input-parent">
+                                <label for="email">Enter your email </label>
+                                <input defaultValue={loginEmail}
+                                    {...register("userEmail", {
+                                        required: true,
+                                        pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                                    })}
+                                />
+                                {errors?.userEmail?.type === "pattern" && <span> <i class="fas fa-exclamation-triangle"></i> Enter valid Email</span>}
+                                {errors?.userEmail?.type === "required" && <span><i class="fas fa-exclamation-triangle"></i> This field is required</span>}
+                            </div>
 
-                <>    <div class="bat-login-form">
-                    <h3 class="bat-m-tb-1r bat-txt-center ">Welcome Back 👋🏻</h3>
-                    <form onSubmit={handleSubmit(signInUser)}>
-                        <div class="input-parent">
-                            {/* <input type="text" value={loginEmail} required onChange={(event) => {
-                        setLoginEmail(event.target.value);
-                    }} />
-                    <label for="email">Enter your email </label> */}
-
-                            <label for="email">Enter your email </label>
-                            <input defaultValue={loginEmail}
-                                {...register("userEmail", {
-                                    required: true,
-                                    pattern: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-                                })}
-                            />
-                            {errors?.userEmail?.type === "pattern" && <span> <i class="fas fa-exclamation-triangle"></i> Enter valid Email</span>}
-                            {errors?.userEmail?.type === "required" && <span><i class="fas fa-exclamation-triangle"></i> This field is required</span>}
-
-
-
+                            <div class="input-parent">
+                                <label for="Password">Enter Password </label>
+                                <input defaultValue={loginPassword} type={passVisible ? "password" : "text"} {...register("userPassword", { minLength: 6, required: true, pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/ })} />
+                                <div onClick={() => setPassVisible(!passVisible)} class={`pointer hide-pass-icon text-color-grey fas ${passVisible ? "fa-eye-slash" : "fa-eye"}`}></div>
+                                {errors.userPassword && (
+                                    <span><i class="fas fa-exclamation-triangle"></i> Password must be at least 6 digit</span>
+                                )}
+                                {errors?.userPassword?.type === "required" && <span></span>}
+                            </div>
+                            <p class="small-text">
+                                <input type="checkbox" onClick={(e) => {
+                                    setRememberMe(e.target.checked);
+                                }} /> Remember me.
+                            </p>
+                            <button class="bat-btn plcae-order-btn bat-br-3px bat-btn-primary">
+                                Log In
+                            </button>
+                        </form>
+                        <div class="bat-fs-1r bat-txt-center margin-t-1 bat-pad-tb-1 ">
+                            new here?            <span class="text-color-primary bat-fw-700 pointer" onClick={() => setNewUser(true)}> Sign Up! </span>
                         </div>
-
-                        <div class="input-parent">
-                            {/* <input required value={loginPassword}
-                        type={passVisible ? "password" : "text"}
-                        onChange={(event) => {
-                            setLoginPassword(event.target.value);
-                        }} /> */}
-                            <label for="Password">Enter Password </label>
-                            <input defaultValue={loginPassword} type={passVisible ? "password" : "text"} {...register("userPassword", { minLength: 6, required: true, pattern: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/ })} />
-                            <div onClick={() => setPassVisible(!passVisible)} class={`pointer hide-pass-icon text-color-grey fas ${passVisible ? "fa-eye-slash" : "fa-eye"}`}></div>
-                            {errors.userPassword && (
-                                <span><i class="fas fa-exclamation-triangle"></i> Password must be at least 6 digit</span>
-                            )}
-                            {errors?.userPassword?.type === "required" && <span></span>}
-
-                        </div>
-                        <p class="small-text">
-                            <input type="checkbox" onClick={(e) => {
-                                setRememberMe(e.target.checked);
-                            }} /> Remember me.
-                        </p>
-                        <button class="bat-btn plcae-order-btn bat-br-3px bat-btn-primary">
-                            Log In
-                        </button>
-                    </form>
-                    <div class="bat-fs-1r bat-txt-center margin-t-1 bat-pad-tb-1 ">
-                        new here?            <span class="text-color-primary bat-fw-700 pointer" onClick={() => setNewUser(true)}> Sign Up! </span>
+                        <button class="bat-btn bat-br-3px bat-btn-primary-outlined bat-fs-14px" style={{ color: "white" }} onClick={loginAsGuest}>Sign in as Guest</button>
                     </div>
-                    <button class="bat-btn bat-br-3px bat-btn-primary-outlined bat-fs-14px" style={{ color: "white" }} onClick={loginAsGuest}>Sign in as Guest</button>
-                </div>
                 </>)
                 : <App />}
 
